c8f8078212172a5ab69156a15f46781bd1822731
Margarita Nudel-Linetsky <rmlinetsky@gmail.com>
2016-11-11 20:37:54 -0500

    changed hour interval

--- manifests/omsa_monitor.pp
+++ manifests/omsa_monitor.pp
@@ -2,7 +2,7 @@ class omsa_monitor {
 cron { 'omsa_monitor':
   command => '/usr/bin/bash /data/monitoring/scripts/run_omreport.sh',
   user    => 'root',
-  hour    => 2,
+  hour    => 19,
   minute  => 0,
 }
 }
