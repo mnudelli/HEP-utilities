95fdf29fa30282b6c90b0db7870d20a78b722bb7
Margarita Nudel-Linetsky <rmlinetsky@gmail.com>
2016-11-11 20:38:48 -0500

    changed it to 22 for testing

--- manifests/omsa_monitor.pp
+++ manifests/omsa_monitor.pp
@@ -2,7 +2,7 @@ class omsa_monitor {
 cron { 'omsa_monitor':
   command => '/usr/bin/bash /data/monitoring/scripts/run_omreport.sh',
   user    => 'root',
-  hour    => 19,
+  hour    => 22,
   minute  => 0,
 }
 }
